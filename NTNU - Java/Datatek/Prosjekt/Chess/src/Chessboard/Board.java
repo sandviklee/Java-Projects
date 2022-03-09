3b72910d29ec46e82da374af261cc56ae3ea02cf
SaiYueWasTaken <74411800+SaiYueWasTaken@users.noreply.github.com>
2022-03-09 10:47:31 +0100

    Java Prosjekt

--- NTNU - Java/Datatek/Prosjekt/Chess/src/Chessboard/Board.java
+++ NTNU - Java/Datatek/Prosjekt/Chess/src/Chessboard/Board.java
@@ -1,5 +1,11 @@
 package Chessboard;
 
+import Pieces.BasePiece;
+import Pieces.Pawn;
+
 public class Board {
-    
+    public static void main(String[] args) {
+        BasePiece p1 = new Pawn(0, 1, 1);
+        System.out.println(p1.getPieceName());
+    }
 }
